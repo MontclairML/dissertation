a22a2bf78d56def25de65f5d6dc9a4b1a054dc8b
Diego Figueiras <67602522+MontclairML@users.noreply.github.com>
2024-03-22 16:30:20 -0400

    Add files via upload

--- analysis.R
+++ analysis.R
@@ -16,7 +16,7 @@ data[2:106]<- mutate_all(data[2:106], function(x) as.numeric(as.character(x)))
 
 # Identify numeric columns
 numeric_columns <- sapply(data, is.numeric)
-numeric_columns$DLEAVING<-as.factor(as.character(numeric_columns$DLEAVING))
+
 
 # Median imputation for each numeric column
 for (col_name in names(numeric_columns)[numeric_columns]) {
@@ -28,6 +28,17 @@ for (col_name in names(numeric_columns)[numeric_columns]) {
 }
 
 data$DLEAVING<-as.factor(as.character(data$DLEAVING))
+data<- data%>%select(1:Q14, Q16:DLEAVING)
+data$workXP<-data%>%select(Q1:Q13)%>%rowMeans()
+data$workUnit<-data%>%select(Q14:Q24)%>%rowMeans()
+data$workOffice<-data%>%select(Q25:Q34)%>%rowMeans()
+data$workOrg<-data%>%select(Q35:Q44)%>%rowMeans()
+data$workSuper<-data%>%select(Q45:Q54)%>%rowMeans()
+data$leadership<-data%>%select(Q55:Q64)%>%rowMeans()
+data$workSat<-data%>%select(Q65:Q70)%>%rowMeans()
+data$DEI<-data%>%select(Q71:Q84)%>%rowMeans()
+data$employeeXP<-data%>%select(Q85:Q89)%>%rowMeans()
+data$flexibility<-data%>%select(Q90:Q99)%>%rowMeans()
 
 #####################################################################################
 
@@ -37,13 +48,13 @@ library(caret)
 train.control <- trainControl(method = "cv", 
                               number = 5,verboseIter = TRUE)
 metric="Accuracy"
-train_data<-data[2:106]
+train_data<-data%>%select(DLEAVING:flexibility)
 
 # Split the data into training and testing sets
-set.seed(123) # For reproducibility
-data_split <- initial_split(data, prop = 0.8)
-train_data <- training(data_split)
-test_data <- testing(data_split)
+# set.seed(123) # For reproducibility
+# data_split <- initial_split(data, prop = 0.8)
+# train_data <- training(data_split)
+# test_data <- testing(data_split)
 # Train the model
 set.seed(9)
 model.lm <- train(DLEAVING ~., data = train_data, method = "lm", preProcess = c("medianImpute","center","scale"), metric=metric,
@@ -51,11 +62,11 @@ model.lm <- train(DLEAVING ~., data = train_data, method = "lm", preProcess = c(
 summary(model.lm)
 
 set.seed(7)
-model.glmnet<-train(DLEAVING~.,data=train_data,tuneGrid=expand.grid(alpha=0:1,lambda=seq(0.0001,1,length=20)),
+model.nn<-train(DLEAVING~.,data=train_data,tuneGrid=expand.grid(alpha=0:1,lambda=seq(0.0001,1,length=20)),
                     method="glmnet", metric=metric, trControl=train.control)
 
 set.seed(6)
-model.rf<-train(DLEAVE ~., tuneLength=10, data = train_data, method = "ranger", metric=metric,
+model.rf<-train(DLEAVING ~., tuneLength=10, data = train_data, method = "ranger", metric=metric,
                 trControl = train.control)
 summary(model.rf)
 set.seed(4)
